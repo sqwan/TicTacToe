0a921c10d464f119f8f2df590818763b6104f26f
Jan Höck <dev-jho@flowfact.com>
2015-11-13 15:07:54 +0100

    Fix

--- ToeTacTic/Form1.cs
+++ ToeTacTic/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +12,8 @@ namespace ToeTacTic
 {
     public partial class Form1 : Form
     {
+        private GameBoardControl control;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,23 +21,14 @@ namespace ToeTacTic
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //GameVerifier gv = new GameVerifier();
-            //gv.TestIsGameOver();
+            control = new GameBoardControl(100, 100);
+            this.Controls.Add(control);
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            Graphics graphics = panel1.CreateGraphics();
-            Brush black = new SolidBrush(Color.Black);
-            Pen blackPen = new Pen(black, 2);
-
-            for (int row = 0; row < 3; row++)
-            {
-                for (int column = 0; column < 3; column++)
-                {
-                    graphics.DrawRectangle(blackPen, 5 + row * 55, 5 + column * 55, 50, 50);
-                }
-            }
+        private void Form1_Resize(object sender, EventArgs e) {
+            control.Width = Width/2;
+            control.Height = Height/2;
+            control.Invalidate();
         }
     }
 }
